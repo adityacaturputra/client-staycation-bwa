5cbe7ebfe6e3c6cb936cb4a0e8f05c9a2d31c0a2
adityacaturputra <adityacaturputra25@gmail.com>
2021-07-28 19:13:27 +0700

    Complete DetailsPage Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
-import Example from "elements/Breadcrumb/Example";
+import DetailsPage from "pages/DetailsPage";
 import "assets/scss/style.scss";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
-        <Route exact path="/example" component={Example}></Route>
+        <Route exact path="/properties/:id" component={DetailsPage}></Route>
       </Router>
     </div>
   );
