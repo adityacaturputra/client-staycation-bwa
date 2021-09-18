5b0b81d54419bdbd00dcabcfd916cee7468c27de
adityacaturputra <adityacaturputra25@gmail.com>
2021-09-18 15:50:56 +0700

    axios interceptor

--- src/store/actions/checkout.js
+++ src/store/actions/checkout.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "configs/axios";
 import { CHECKOUT_BOOKING } from "store/types";
 
 export const checkoutBooking = (payload) => (dispatch) => {
@@ -9,5 +9,5 @@ export const checkoutBooking = (payload) => (dispatch) => {
 }
 
 export const submitBooking = (payload) => () => {
-    return axios.post(`https://admiin-staycation.herokuapp.com/api/v1/member/booking-page`, payload, {headers: {contentType: "multipart/form-data"}})
+    return axios.post(`/booking-page`, payload, {headers: {contentType: "multipart/form-data"}})
 }
